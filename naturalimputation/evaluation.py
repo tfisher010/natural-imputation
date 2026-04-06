df9e92222b661275f590ff4914e30dadda0ea82c
Ted Fisher <tfisher010@gmail.com>
2026-04-05 19:16:51 -0700

    remove duplicated inference

--- naturalimputation/evaluation.py
+++ naturalimputation/evaluation.py
@@ -5,9 +5,10 @@ import numpy as np
 
 def evaluate(X, y, test):
     model = LogisticRegression().fit(X=X[~test], y=y[~test])
+    preds = model.predict_proba(X)[:, 1]
     return np.array(
         [
-            roc_auc_score(y_true=y[~test], y_score=model.predict_proba(X)[:, 1][~test]),
-            roc_auc_score(y_true=y[test], y_score=model.predict_proba(X)[:, 1][test]),
+            roc_auc_score(y_true=y[~test], y_score=preds[~test]),
+            roc_auc_score(y_true=y[test], y_score=preds[test]),
         ]
     )
